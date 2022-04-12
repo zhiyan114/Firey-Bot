e11abbc4ead77a527727b01c98d7611235b67525
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2022-04-12 16:42:40 -0400

    Completed Rules Check Role

---
+++ commands/kick.js
@@ -0,0 +1,5 @@
+module.exports = {
+    command: null,
+    function: null,
+    disabled: true,
+}
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //const sentry = require('@sentry/node');
 const fs = require('fs');
 const path = require('path');
-const { Client, Intents, MessageEmbed } = require('discord.js');
+const { Client, Intents, MessageEmbed, MessageSelectMenu } = require('discord.js');
 require("./CmdRegister.js");
 //const db = require("./DatabaseInit.js");
 
@@ -17,7 +17,7 @@ fs.readdirSync(dir).forEach(file => {
 });
 
 /* Client Loader */
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS], partials: ["CHANNEL"] });
 
 client.on('ready', () => {
   //client.user.setActivity('everyone below me', { type: 'WATCHING' });
@@ -38,16 +38,41 @@ client.on('interactionCreate', async interaction => {
     await commandList[interaction.commandName](interaction);
   }
 });
-client.on('message',async(message)=>{
+client.on('guildMemberAdd',async member => {
+  const embed = new MessageEmbed()
+    .setTitle("Welcome to the server!")
+    .setDescription(`Welcome to the Derg server, ${member.user.username}! Please read the rules and reply \`I agree with the rules\` to get access to the server.`)
+    .setColor("#0000FF")
+  member.send({embeds: [embed]});
+});
+client.on('messageCreate',async(message)=>{
   // Check if the message is from this client
   if (message.author.id === client.user.id) return;
   // Check if the message is from DM
-  if (message.channel.type != 'dm') return;
+  if (message.channel.type != 'DM') return;
   // Get member from user ID
   const guild = await client.guilds.cache.find(opt=>opt.id == "906899666656956436")
-  const member = guild.members.fetch();
-  if(message.content.toLowerCase() === "I agree with the rules") {
+  const member = (await guild.members.fetch()).find(opt=>opt.id == message.author.id);
+  // Check if the member has a role
+  if (!member.roles.cache.find(opt=>opt.id == "907768073442983966")) {
+    // Create Embed
+    const embed = new MessageEmbed()
+      .setTitle("Rules Verification")
+      .setFooter({text: "Administration Team"})
+      .setTimestamp();
+    if(message.content.toLowerCase() === "I agree with the rules") {
+      embed.setColor("#00FF00")
+      embed.setDescription("Thank you for reading and agreeing with the rules. You should now have full access to the server.");
+      await member.roles.add(guild.roles.cache.find(opt=> opt.id == '907768073442983966'), "User Read the rules");
+      await message.reply({embeds: [embed]});
+      return;
+    }
+    embed.setColor("#FF0000");
+    embed.setDescription("You must agree with the rules to join the server! To do so, please reply: `I agree with the rules`");
+    await message.reply({embeds: [embed]});
+    return
   }
+  message.reply("You've already verified, there is nothing you can do in my DM right now. Sorry ;(");
 });
 
 client.login('OTYzNDIwODYyNDgzMTQ0NzM1.YlV1mQ.06-w0uXaPMtmjKIDgs2gFgoCjMQ');
