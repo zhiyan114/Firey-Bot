4c698d5924c31258662f761c2a62a0125690615e
zhiyan114 <20156291+zhiyan114@users.noreply.github.com>
2022-04-12 15:30:12 -0400

    Rule-Check CKPT 1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 //const sentry = require('@sentry/node');
 const fs = require('fs');
 const path = require('path');
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageEmbed } = require('discord.js');
 require("./CmdRegister.js");
 //const db = require("./DatabaseInit.js");
 
@@ -17,7 +17,7 @@ fs.readdirSync(dir).forEach(file => {
 });
 
 /* Client Loader */
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS] });
 
 client.on('ready', () => {
   //client.user.setActivity('everyone below me', { type: 'WATCHING' });
@@ -25,7 +25,7 @@ client.on('ready', () => {
   client.user.setPresence({
     status: "idle",
     activities: [{
-      name: "all furries that are here",
+      name: "all furries UwU",
       type: "WATCHING",
     }]
   })
@@ -36,6 +36,17 @@ client.on('interactionCreate', async interaction => {
   if (interaction.isCommand()) {
     if (!commandList[interaction.commandName]) return;
     await commandList[interaction.commandName](interaction);
+  }
+});
+client.on('message',async(message)=>{
+  // Check if the message is from this client
+  if (message.author.id === client.user.id) return;
+  // Check if the message is from DM
+  if (message.channel.type != 'dm') return;
+  // Get member from user ID
+  const guild = await client.guilds.cache.find(opt=>opt.id == "906899666656956436")
+  const member = guild.members.fetch();
+  if(message.content.toLowerCase() === "I agree with the rules") {
   }
 });
 
