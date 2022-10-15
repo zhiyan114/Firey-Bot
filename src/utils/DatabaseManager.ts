f5deceaf27f881eea26d6bcb08f12d2aeee284aa
zhiyan114 <zhiyan114@zhiyan114.com>
2022-10-15 13:38:55 -0400

    Remove X509 database connection due to lack of env variable support

--- src/utils/DatabaseManager.ts
+++ src/utils/DatabaseManager.ts
@@ -1,8 +1,7 @@
 import { Db, MongoClient, ServerApiVersion } from "mongodb";
 import { LogType, sendLog } from "./eventLogger";
+
 export const dbclient = new MongoClient(process.env['MONGODB_CONN'] as string, {
-  sslKey: process.env['MONGODB_CERT'],
-  sslCert: process.env['MONGODB_CERT'],
   serverApi: ServerApiVersion.v1,
 });
 let _isConnected = false;
