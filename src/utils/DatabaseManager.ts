d5ba91b1cfb3c06ed860abe8f4bd1c894b5c7b98
zhiyan114 <zhiyan114@zhiyan114.com>
2022-10-15 15:03:31 -0400

    2nd solution for cron cert check error

--- src/utils/DatabaseManager.ts
+++ src/utils/DatabaseManager.ts
@@ -3,7 +3,7 @@ import { LogType, sendLog } from "./eventLogger";
 
 export const dbclient = new MongoClient(process.env['MONGODB_CONN'] as string, {
   serverApi: ServerApiVersion.v1,
-  tlsAllowInvalidCertificates: true, // Temp Bypass since cron job throws validation error wwhen it starts.
+  checkServerIdentity: ()=>undefined, // Bypass server checks to hopefully fix cron error
 });
 let _isConnected = false;
 export let isConnected = () => _isConnected;
