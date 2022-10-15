d509115e78468016ed078e52231948e6b1b74dd5
zhiyan114 <zhiyan114@zhiyan114.com>
2022-10-15 13:44:49 -0400

    Defined slash builder for the TODO command

--- src/commands/getPoints.ts
+++ src/commands/getPoints.ts
@@ -1,1 +1,18 @@
 // @TODO: Show individual's balance privately?
+
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Client, CommandInteraction } from 'discord.js';
+/* Command Builder */
+const GetPointsCmd = new SlashCommandBuilder()
+    .setName('getPoints')
+    .setDescription(`Showw your points privately`)
+
+/* Function Builder */
+const GetPointsFunc = async (interaction : CommandInteraction, client : Client) => {
+}
+
+export default {
+    command: GetPointsCmd,
+    function: GetPointsFunc,
+    disabled: true,
+}
--- src/commands/leaderboard.ts
+++ src/commands/leaderboard.ts
@@ -1,1 +1,18 @@
 // @TODO: Show global top 10 points holder.
+
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { Client, CommandInteraction } from 'discord.js';
+/* Command Builder */
+const leaderboardCmd = new SlashCommandBuilder()
+    .setName('leaderboard')
+    .setDescription(`Show the top ten points holder`)
+
+/* Function Builder */
+const leaderboardFunc = async (interaction : CommandInteraction, client : Client) => {
+}
+
+export default {
+    command: leaderboardCmd,
+    function: leaderboardFunc,
+    disabled: true,
+}
