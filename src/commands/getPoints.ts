fb517812492246bfdd31d3c5b7f99be96c7555e7
zhiyan114 <zhiyan114@zhiyan114.com>
2022-10-16 18:24:23 -0400

    getPoints: Completed

--- src/commands/getPoints.ts
+++ src/commands/getPoints.ts
@@ -1,18 +1,37 @@
 // @TODO: Show individual's balance privately?
 
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { Client, CommandInteraction } from 'discord.js';
+import { Client, CommandInteraction, EmbedBuilder } from 'discord.js';
+import Mongoose from 'mongoose';
+import { econSchema, econType } from '../services/EconomyHandler';
+import { isConnected } from '../utils/DatabaseManager';
+
 /* Command Builder */
 const GetPointsCmd = new SlashCommandBuilder()
     .setName('getpoints')
-    .setDescription(`Show your points privately`)
+    .setDescription(`Show your total points publically or privately`)
+    .addBooleanOption(opt=>
+        opt.setName("ephemeral")
+            .setDescription("Whether to show your points privately. Default is true.")
+            .setRequired(false)
+    )
 
 /* Function Builder */
 const GetPointsFunc = async (interaction : CommandInteraction, client : Client) => {
+    const isEphmeral = interaction.options.get('ephemeral', false)?.value as boolean ?? true;
+    if(!isConnected()) return await interaction.reply({content: "Unfortunately the database is not connected, please report this issue.", ephemeral: true});
+    await interaction.deferReply();
+    const econModel = Mongoose.model<econType>("economy",econSchema);
+    const userEconData = await econModel.findOne({_id: interaction.user.id});
+    const embed = new EmbedBuilder();
+    embed.setTitle(`Your Points`);
+    embed.setAuthor({name: interaction.user.tag, iconURL: interaction.user.avatarURL() ?? interaction.user.defaultAvatarURL});
+    embed.setTimestamp();
+    await interaction.followUp({embeds:[embed], ephemeral: isEphmeral});
 }
 
 export default {
     command: GetPointsCmd,
     function: GetPointsFunc,
-    disabled: true,
+    disabled: false,
 }
