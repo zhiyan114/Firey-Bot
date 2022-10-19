b81abac39117e6c7852b4ad578a6f3515fcd8b7c
zhiyan114 <zhiyan114@zhiyan114.com>
2022-10-19 19:08:54 -0400

    twitchStream: Log error if API cannot be fetched

--- src/utils/twitchStream.ts
+++ src/utils/twitchStream.ts
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import events from 'events';
 import { twitch } from '../config';
+import { LogType, sendLog } from './eventLogger';
 
 const callback = new events({});
 
@@ -26,29 +27,43 @@ type getStreamData = {
     is_mature: boolean,
     
 }
-type twitchGetStreamType = {
+export type twitchGetStreamType = {
     data: getStreamData[],
     pagination: stringObjectType,
 }
 
-export let alreadyStreaming = false;
+let alreadyStreaming = false;
+export const isStreaming = () => alreadyStreaming;
 
+let errLogged = false;
+let lastCallSuccess = false;
 // Twitch is making things harder than it needs to smh
 const mainCheck = () => {
+    if(!lastCallSuccess && !errLogged) sendLog(LogType.Warning, "Twitch API may be down, you'll be notified again once the API call is successful")
+    lastCallSuccess = false;
     axios.get<twitchGetStreamType>(`https://api.twitch.tv/helix/streams?user_login=${twitch.channels[0]}`,{
         headers: {
             "client-id": "q6batx0epp608isickayubi39itsckt", // Just using someone else's client ID
             "Authorization": `Bearer ${process.env['TWITCH_TOKEN']}`
         }
     }).then(res=>{
+        if(res.status != 200) {
+            errLogged = true;
+            lastCallSuccess = false;
+            sendLog(LogType.Warning, `Twitch API is responding with ${res.status} with message \`${JSON.stringify(res.data)}\``);
+            return;
+        }
         if(res.data.data.length > 0 && !alreadyStreaming) {
             alreadyStreaming = true;
-            callback.emit('start');
+            callback.emit('start', res.data);
         }
-        if(res.data.data.length == 0 && alreadyStreaming) {
+        else if(res.data.data.length == 0 && alreadyStreaming) {
             alreadyStreaming = false;
             callback.emit('end');
         }
+        if(!lastCallSuccess) sendLog(LogType.Info, `Twitch API call can now be completed!`);
+        lastCallSuccess = true;
+        errLogged = false;
     })
     // Check it again every 30 seconds regardless if the api fails or not
     setTimeout(mainCheck,30000);
