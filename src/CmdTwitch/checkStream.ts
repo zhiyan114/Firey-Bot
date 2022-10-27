d77f5ba4c533b3b1b89d63fbbd1f793846fe6b86
zhiyan114 <zhiyan114@zhiyan114.com>
2022-10-26 22:40:48 -0400

    Fixed some bad twitch command API calls

--- src/CmdTwitch/checkStream.ts
+++ src/CmdTwitch/checkStream.ts
@@ -5,7 +5,7 @@ import { twitchCmdType } from './index';
 const checkStream : twitchCmdType ={
     name: "checkstream",
     func: async (data) => {
-        await data.client.say(data.channel, `@${data.user.tags.username}, the stream state is ${isStreaming()}`);
+        await data.client.say(data.channel, `@${data.user.username}, the stream state is ${isStreaming()}`);
     } 
  }
 
--- src/CmdTwitch/gePoints.ts
+++ src/CmdTwitch/gePoints.ts
@@ -5,18 +5,18 @@ import { twitchCmdType } from './index';
 const getPointsFunc : twitchCmdType ={
     name: "getpoints",
     func: async (data) => {
-        const args = data.args;
-        if(data.authUsers[data.user.tags['user-id']] == "-1") return data.client.say(data.channel,`@${data.user.tags.username}, your account is not linked yet. Do that first then try again.`);
-        let discordUserID = data.authUsers[data.user.tags['user-id']];
+        if(!data.user['user-id']) return;
+        if(data.authUsers[data.user['user-id']] == "-1") return data.client.say(data.channel,`@${data.user.username}, your account is not linked yet. Do that first then try again.`);
+        let discordUserID = data.authUsers[data.user['user-id']];
         if(!discordUserID) {
             // Get the user ID since it's not available when the stream is offline
-            const userData = await econModel.findOne({"twitch.ID": data.user.tags['user-id']});
+            const userData = await econModel.findOne({"twitch.ID": data.user['user-id']});
             if(userData) discordUserID = userData._id
         }
         // Fetch the points
         const econData = await econModel.findOne({_id: discordUserID})
-        if(!econData) return await data.client.say(data.channel,`${data.user.tags.username}, you have 0 points!`);
-        await data.client.say(data.channel,`@${data.user.tags.username}, you have ${econData?.points} points!`);
+        if(!econData) return await data.client.say(data.channel,`${data.user.username}, you have 0 points!`);
+        await data.client.say(data.channel,`@${data.user.username}, you have ${econData?.points} points!`);
     } 
  }
 
