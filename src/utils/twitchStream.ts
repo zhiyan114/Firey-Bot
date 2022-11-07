4b0eb9562261dba38682462d7eb699ac2de05b6a
zhiyan114 <zhiyan114@zhiyan114.com>
2022-11-07 16:53:52 -0500

    twitchStream: Fixed bad emit

--- src/utils/twitchStream.ts
+++ src/utils/twitchStream.ts
@@ -4,9 +4,6 @@ import axios from 'axios';
 import events from 'events';
 import { LogType, sendLog } from './eventLogger';
 import https from 'https'; 
-import http from 'http';
-
-export const streamStatus = new events({});
 
 // Type Reference: https://dev.twitch.tv/docs/api/reference#get-streams
 interface stringObjectType {
@@ -38,6 +35,8 @@ export type twitchGetStreamType = {
 export declare interface twitchClient {
     on(event: 'start', listener: (data: getStreamData) => void): this;
     on(event: 'end', listener: () => void): this;
+    emit(eventName: 'start', data: getStreamData): boolean;
+    emit(eventName: 'end'): boolean;
 }
 export class twitchClient extends events.EventEmitter {
     private token: string;
@@ -50,7 +49,7 @@ export class twitchClient extends events.EventEmitter {
         this.token = token ?? process.env['TWITCH_TOKEN'] ?? "";
         if(this.token == "") throw new tClientError("TwitchClient's oauth token is not properly supplied", "BadToken");
         this.channel = channelName;
-        this.cooldown = cooldown ?? 30000; // Check every 30 seconds by default
+        this.cooldown = cooldown ?? 30000; // Check every 20 seconds by default
         this.mainCheck();
     }
     private mainCheck = async () => {
@@ -60,9 +59,8 @@ export class twitchClient extends events.EventEmitter {
                     "client-id": "q6batx0epp608isickayubi39itsckt", // Just using someone else's client ID
                     "Authorization": `Bearer ${this.token}`,
                 },
-                timeout: 60000,
+                timeout: 20000,
                 httpsAgent: new https.Agent({ keepAlive: true }),
-                httpAgent: new http.Agent({ keepAlive: true }),
             });
             if(serverResponse.status != 200) {
                 this.errLogged = true;
@@ -71,11 +69,11 @@ export class twitchClient extends events.EventEmitter {
             }
             if(serverResponse.data.data.length > 0 && !this.isStreaming) {
                 this.isStreaming = true;
-                streamStatus.emit('start',  serverResponse.data.data[0]);
+                this.emit('start',  serverResponse.data.data[0]);
             }
             else if(serverResponse.data.data.length == 0 && this.isStreaming) {
                 this.isStreaming = false;
-                streamStatus.emit('end');
+                this.emit('end');
             }
             if(this.errLogged) sendLog(LogType.Info, `Twitch API call can now be completed!`);
             this.errLogged = false;
