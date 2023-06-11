444f9595c3174301bc7addc13d2529f32ed39f94
zhiyan114 <zhiyan114@zhiyan114.com>
2023-06-10 20:10:04 -0400

    Refactor(GlobalCmdRegister): Removed unnecessary array maps

--- src/globalCmdRegister.ts
+++ src/globalCmdRegister.ts
@@ -6,32 +6,25 @@
 import path from "path";
 import fs from 'fs';
 import { ICommand } from "./interface";
-import { REST, Routes } from "discord.js";
+import { REST, RESTPostAPIChatInputApplicationCommandsJSONBody, Routes } from "discord.js";
 import { botToken, clientID } from "./config";
 
-// Key type
-interface ICommandList {
-    [key: string]: ICommand;
-}
-
 /* Load all the internal commands */
-console.log("Loading Commands")
-const commandList : ICommandList = {};
+console.log("Loading Commands...")
+const commandList : RESTPostAPIChatInputApplicationCommandsJSONBody[] = [];
 const cmdDir = path.join(__dirname, './', 'commands');
 for(let file of fs.readdirSync(cmdDir)) {
     if (file.endsWith('.js')) {
         const cmdModule : ICommand = require(path.join(cmdDir, file)).default;
         if(!cmdModule) continue;
-        if(!cmdModule.disabled) commandList[cmdModule.command.name] = cmdModule;
+        if(!cmdModule.disabled) commandList.push(cmdModule.command.toJSON());
     }
 }
 
-// Add the commands to the server
-console.log("Registering Commands")
+// Enable/Update the command globally
+console.log("Registering Commands...")
 const rest = new REST({ version: '10' }).setToken(botToken);
 rest.put(
     Routes.applicationCommands(clientID),
-    { body: Object.values(commandList).map(cmd=>{ if(!cmd.disabled) return cmd.command.toJSON() }) },
-);
-
-console.log("Done")
+    { body: commandList },
+).then(()=> console.log("Done"));
