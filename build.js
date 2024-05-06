7c9c63c3dcc53180ca52fb0d12ef1a022d1411b0
zhiyan114 <zhiyan114@zhiyan114.com>
2024-05-06 10:06:00 -0400

    Complete new esbuild method

---
+++ build.js
@@ -0,0 +1,55 @@
+const esbuild = require('esbuild');
+const fs = require('fs');
+const path = require('path');
+
+const basePath = "src";
+
+function getAllFilesInFolder(folderPath, filesArray = []) {
+  const files = fs.readdirSync(folderPath);
+
+  files.forEach(file => {
+    const filePath = path.join(folderPath, file).replace(/\\/g, '/');
+    const stats = fs.statSync(filePath);
+
+    if (stats.isDirectory())
+      getAllFilesInFolder(filePath, filesArray);
+    else
+      if(filePath.endsWith('.ts') || filePath.endsWith('.js'))
+        filesArray.push(filePath);
+  });
+  return filesArray;
+}
+
+// Run Build
+const out = esbuild.buildSync({
+  entryPoints: getAllFilesInFolder(basePath),
+  minify: true,
+  minifyIdentifiers: true,
+  minifySyntax: true,
+  minifyWhitespace: true,
+  platform: "node",
+  format: "cjs",
+  packages: "external",
+  sourcemap: true,
+  metafile: true,
+  outdir: "dist",
+});
+if(out.errors.length > 0)
+  console.error(`Build Failed: ${JSON.stringify(out.errors)}`);
+
+// Copy over config.json
+fs.copyFileSync(path.join(basePath, "config.json"), path.join("dist", "config.json"));
+
+// Compute build size
+let buildSize = 0;
+const sizeUnits = ["bytes", "KB", "MB", "GB", "TB", "Yeah Not gonna happen"];
+let sizeUnitIndex = 0;
+if(out.metafile?.outputs)
+  for (const file of Object.keys(out.metafile.outputs))
+    buildSize += out.metafile.outputs[file].bytes;
+while (buildSize > 1024) {
+  buildSize /= 1024;
+  sizeUnitIndex++;
+}
+
+console.log(`Build Success! Size: ${(buildSize).toFixed(2)} ${sizeUnits[sizeUnitIndex]}`);
