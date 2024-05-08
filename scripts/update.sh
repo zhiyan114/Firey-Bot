dc44cf10ad98e25443920e3659fd96dcab761327
zhiyan114 <zhiyan114@zhiyan114.com>
2024-05-08 00:41:54 -0400

    all in one update script

---
+++ scripts/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## This is a all in one updater script for the target machine
+## DO NOT USE IN Dockerfile
+
+## Use for clean up ONLY:
+# docker system prune -a --volumes
+
+cd ../
+
+# Only build if the master branch is out-of-date
+pull_output=$(git pull)
+if [[ "$pull_output" == *"Already up to date."* ]]; then
+    echo "Already up to date."
+    exit 0
+fi
+
+# BUILDKIT_PROGRESS=plain
+doppler run -- docker compose build
+docker compose down
+doppler run -- docker compose up -d
