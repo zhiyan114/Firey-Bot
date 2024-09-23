caad995240cb906238ea2bc24abdbf1c1bd24561
zhiyan114 <zhiyan114@zhiyan114.com>
2024-09-23 13:01:15 -0400

    withScope most of the callback event for discordEvent

--- src/events/discordEvents.ts
+++ src/events/discordEvents.ts
@@ -44,10 +44,11 @@ export class DiscordEvents extends baseEvent {
       scope.setUser({
         id: interaction.user.id,
         username: interaction.user.username,
-        isStaff: gMember?.roles.cache.some(r=>r.id === this.client.config.adminRoleID) ?? false,
-        isVerified: gMember?.roles.cache.some(r=>r.id === this.client.config.newUserRoleID) ?? false
+        isStaff: gMember?.roles.cache.some(r=>r.id === this.client.config.adminRoleID) ?? "unknown",
+        isVerified: gMember?.roles.cache.some(r=>r.id === this.client.config.newUserRoleID) ?? "unknown"
       });
       scope.setTag("platform", "discord");
+      scope.setTag("eventType", "interactionCreate");
 
       if(interaction.isCommand() || interaction.isContextMenuCommand())
         return await this.commandHandler.commandEvent(interaction);
@@ -59,70 +60,101 @@ export class DiscordEvents extends baseEvent {
   }
 
   private async messageCreate(message: Message) {
-    // Channel Checks
-    if(message.author.bot) return;
-    const channel = message.channel;
-    if(channel.type !== ChannelType.GuildText) return;
+    await withScope(async (scope) => {
+      scope.setUser({
+        id: message.author.id,
+        username: message.author.username,
+        isStaff: message.member?.roles.cache.some(r=>r.id === this.client.config.adminRoleID) ?? "unknown",
+        isVerified: message.member?.roles.cache.some(r=>r.id === this.client.config.newUserRoleID) ?? "unknown"
+      });
+      scope.setTag("platform", "discord");
+      scope.setTag("eventType", "messageCreate");
 
-    // Place where user wont be awarded with points
-    const noPointsConf = this.client.config.noPoints;
-    if(noPointsConf.channel.length > 0 && noPointsConf.channel.find(c=>c===channel.id)) return;
-    if(noPointsConf.category.length > 0 && noPointsConf.category.find(c=>channel.parentId === c)) return;
+      // Channel Checks
+      if(message.author.bot) return;
+      const channel = message.channel;
+      if(channel.type !== ChannelType.GuildText) return;
 
-    // Grant points
-    await (new DiscordUser(this.client, message.author)).economy.chatRewardPoints(message.content);
+      // Place where user wont be awarded with points
+      const noPointsConf = this.client.config.noPoints;
+      if(noPointsConf.channel.length > 0 && noPointsConf.channel.find(c=>c===channel.id)) return;
+      if(noPointsConf.category.length > 0 && noPointsConf.category.find(c=>channel.parentId === c)) return;
+
+      // Grant points
+      await (new DiscordUser(this.client, message.author)).economy.chatRewardPoints(message.content);
+    });
   }
 
   private async guildMemberAdd(member: GuildMember) {
-    if(member.user.bot) return;
-    const user = new DiscordUser(this.client, member.user);
-    const channel = await this.client.channels.fetch(this.client.config.welcomeChannelID);
-    if(!channel || channel.type !== ChannelType.GuildText) return;
+    await withScope(async (scope) => {
+      scope.setUser({
+        id: member.user.id,
+        username: member.user.username,
+        isStaff: member.roles.cache.some(r=>r.id === this.client.config.adminRoleID),
+        isVerified: member.roles.cache.some(r=>r.id === this.client.config.newUserRoleID)
+      });
+      scope.setTag("platform", "discord");
+      scope.setTag("eventType", "guildMemberAdd");
 
-    // Create new user entry
-    try {
-      await user.createNewUser();
-    } catch(ex) {
-      if(!(ex instanceof Prisma.PrismaClientKnownRequestError && ex.code === "P2002"))
-        captureException(ex);
-    }
+      if(member.user.bot) return;
+      const user = new DiscordUser(this.client, member.user);
+      const channel = await this.client.channels.fetch(this.client.config.welcomeChannelID);
+      if(!channel || channel.type !== ChannelType.GuildText) return;
 
-    // Send welcome message to user
-    const embed = new EmbedBuilder()
-      .setColor("#00FFFF")
-      .setTitle("Welcome to the server!")
-      .setDescription(`Welcome to the Derg server, ${member.user.username}! Please read the rules and press the confirmation button to get full access.`);
-    try {
-      await member.send({embeds: [embed]});
-    } catch(ex) {
-      if(ex instanceof DiscordAPIError && ex.code === APIErrors.CANNOT_MESSAGE_USER)
-        await channel.send({content:`||<@${member.user.id}> You've received this message here because your DM has been disabled||`, embeds: [embed]});
-      else captureException(ex);
-    }
+      // Create new user entry
+      try {
+        await user.createNewUser();
+      } catch(ex) {
+        if(!(ex instanceof Prisma.PrismaClientKnownRequestError && ex.code === "P2002"))
+          captureException(ex);
+      }
+
+      // Send welcome message to user
+      const embed = new EmbedBuilder()
+        .setColor("#00FFFF")
+        .setTitle("Welcome to the server!")
+        .setDescription(`Welcome to the Derg server, ${member.user.username}! Please read the rules and press the confirmation button to get full access.`);
+      try {
+        await member.send({embeds: [embed]});
+      } catch(ex) {
+        if(ex instanceof DiscordAPIError && ex.code === APIErrors.CANNOT_MESSAGE_USER)
+          await channel.send({content:`||<@${member.user.id}> You've received this message here because your DM has been disabled||`, embeds: [embed]});
+        else captureException(ex);
+      }
     
-    this.client.updateStatus();
+      this.client.updateStatus();
 
-    // Send a welcome banner
-    const BannerBuff = await (new BannerPic()).generate(user.getUsername(), member.user.displayAvatarURL({size: 512}));
-    await channel.send({files: [BannerBuff]});
+      // Send a welcome banner
+      const BannerBuff = await (new BannerPic()).generate(user.getUsername(), member.user.displayAvatarURL({size: 512}));
+      await channel.send({files: [BannerBuff]});
+    });
   }
 
   private async userUpdate(oldUser: User | PartialUser, newUser: User) {
-    if(oldUser.bot) return;
-    if(oldUser.tag === newUser.tag)
-      return;
-    const user = new DiscordUser(this.client, newUser);
+    await withScope(async (scope) => {
+      scope.setUser({
+        id: newUser.id,
+        username: newUser.username,
+      });
+      scope.setTag("platform", "discord");
+      scope.setTag("eventType", "userUpdate");
 
-    // See if we need to update user's rule confirmation date
-    let updateVerifyStatus = false;
-    if(!(await user.getCacheData())?.rulesconfirmedon &&
+      if(oldUser.bot) return;
+      if(oldUser.tag === newUser.tag)
+        return;
+      const user = new DiscordUser(this.client, newUser);
+
+      // See if we need to update user's rule confirmation date
+      let updateVerifyStatus = false;
+      if(!(await user.getCacheData())?.rulesconfirmedon &&
       (await this.client.guilds.cache.find(g=>g.id === this.client.config.guildID)
         ?.members.fetch(newUser))
         ?.roles.cache.find(role=>role.id === this.client.config.newUserRoleID))
-      updateVerifyStatus = true;
+        updateVerifyStatus = true;
     
-    await user.updateUserData({
-      rulesconfirmedon: updateVerifyStatus ? new Date() : undefined
+      await user.updateUserData({
+        rulesconfirmedon: updateVerifyStatus ? new Date() : undefined
+      });
     });
   }
 
@@ -131,41 +163,51 @@ export class DiscordEvents extends baseEvent {
   }
 
   private async voiceStateUpdate(old: VoiceState, now: VoiceState) {
-
-    // Checking to see if the user needs to be reported on the log
-    const config = this.client.config.VCJoinLog;
-    const channel = await this.client.channels.fetch(config.channelID);
-    if(!channel || channel.type !== ChannelType.GuildText) return;
-    if(!now.channel) return;
-    if(old.channel?.id === now.channel.id) return;
-    if(!now.member || now.member.user.bot) return;
-    if(config.excludeChannels.includes(now.channel.id)) return;
-
-    // Prepare embed
-    const embed = new EmbedBuilder()
-      .setColor("#00FFFF")
-      .setTitle("Voice Channel Join")
-      .setThumbnail(now.member.user.displayAvatarURL({size: 512}))
-      .setDescription(`<@${now.member.user.id}> has joined the voice channel <#${now.channel.id}>`)
-      .setTimestamp()
-      .setFields([
-        {
-          name: "User ID",
-          value: now.member.user.id
-        },
-        {
-          name: "Channel ID",
-          value: now.channel.id
-        }
-      ]);
-    
-    // See if username needs to be added as well
-    if(now.member.user.username !== now.member.user.displayName)
-      embed.addFields({
-        name: "Username",
-        value: now.member.user.username
+    await withScope(async (scope) => {
+      scope.setUser({
+        id: now.member?.user.id,
+        username: now.member?.user.username,
+        isStaff: now.member?.roles.cache.some(r=>r.id === this.client.config.adminRoleID),
+        isVerified: now.member?.roles.cache.some(r=>r.id === this.client.config.newUserRoleID)
       });
-    
-    await channel.send({embeds: [embed]});
+      scope.setTag("platform", "discord");
+      scope.setTag("eventType", "voiceStateUpdate");
+
+      // Checking to see if the user needs to be reported on the log
+      const config = this.client.config.VCJoinLog;
+      const channel = await this.client.channels.fetch(config.channelID);
+      if(!channel || channel.type !== ChannelType.GuildText) return;
+      if(!now.channel) return;
+      if(old.channel?.id === now.channel.id) return;
+      if(!now.member || now.member.user.bot) return;
+      if(config.excludeChannels.includes(now.channel.id)) return;
+
+      // Prepare embed
+      const embed = new EmbedBuilder()
+        .setColor("#00FFFF")
+        .setTitle("Voice Channel Join")
+        .setThumbnail(now.member.user.displayAvatarURL({size: 512}))
+        .setDescription(`<@${now.member.user.id}> has joined the voice channel <#${now.channel.id}>`)
+        .setTimestamp()
+        .setFields([
+          {
+            name: "User ID",
+            value: now.member.user.id
+          },
+          {
+            name: "Channel ID",
+            value: now.channel.id
+          }
+        ]);
+
+      // See if username needs to be added as well
+      if(now.member.user.username !== now.member.user.displayName)
+        embed.addFields({
+          name: "Username",
+          value: now.member.user.username
+        });
+
+      await channel.send({embeds: [embed]});
+    });
   }
 }
