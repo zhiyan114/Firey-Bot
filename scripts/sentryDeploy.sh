56e73ae47afa1434f507c88cf8ab4f03d3b42ba4
小兽兽 <zhiyan114@zhiyan114.com>
2025-05-14 13:39:28 -0400

    Chore: Small Maintenance (#254)
    * Small change to versioning
    
    * PKG Upgrade
    
    * fix(vscode): New file should use 2 space indent instead...
    
    * chore: Delete unused files
    
    * feat(build.js): Minify and include config.json as part of build-size calculation

--- scripts/sentryDeploy.sh
+++ scripts/sentryDeploy.sh
@@ -19,5 +19,5 @@ fi
 SRCENV=${ENVIRONMENT:=????}
 
 npx sentry-cli sourcemaps inject ./dist
-npx sentry-cli sourcemaps upload --release="${SRCENV:0:4}@$(git rev-parse --short=7 HEAD)" --dist="$(git rev-parse HEAD)" ./dist
+npx sentry-cli sourcemaps upload --release="${SRCENV:0:4}-$(git rev-parse --short=7 HEAD)" --dist="$(git rev-parse HEAD)" ./dist
 echo Successfully Deployed Source Map
