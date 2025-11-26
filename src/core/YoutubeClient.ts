8c67b26f85f6aa1c799e4606cffe65cf7ea8be96
zhiyan114 <zhiyan114@furries.dev>
2025-11-26 14:28:34 -0500

    PubSubHubBub added sentry error handler

--- src/core/YoutubeClient.ts
+++ src/core/YoutubeClient.ts
@@ -1,10 +1,12 @@
 import type { baseClient } from "./baseClient";
 import type { DiscordClient } from "./DiscordClient";
 import YouTubeNotifier from "../utils/youtube-notifier";
-import Express from 'express';
+import Express, { type NextFunction } from 'express';
 import http from 'http';
 import https from 'https';
 import { YoutubeEvents } from "../events";
+import { getIsolationScope, captureException } from "@sentry/node-core";
+import { httpRequestToRequestData } from "@sentry/core";
 
 /*
 Example Reference
@@ -62,6 +64,16 @@ interface config {
     secret?: string;
 }
 
+// Sentry JS SDK Implementation
+interface MiddlewareError extends Error {
+  status?: number | string;
+  statusCode?: number | string;
+  status_code?: number | string;
+  output?: {
+    statusCode?: number | string;
+  };
+}
+
 // eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export declare interface YoutubeClient extends YouTubeNotifier {
     on(event: "notified", listener: (data: NotifiedEvent) => void): this;
@@ -89,6 +101,8 @@ export class YoutubeClient extends YouTubeNotifier implements baseClient {
     this.discord = config.client;
     this.port = config.Port ?? 80;
     this.express.use(config.Path, this.listener());
+    this.express.get("/test/", this.HealthRoute);
+    this.express.use(this.errMiddleWare); // Sentry Error Handler
     this.httpServer = config.https ? https.createServer(this.express) : http.createServer(this.express);
 
     // Register events
@@ -97,7 +111,6 @@ export class YoutubeClient extends YouTubeNotifier implements baseClient {
   }
 
   public async start() {
-    this.express.get("/test/", this.HealthRoute);
     await new Promise<void>((resolve) => this.httpServer.listen(this.port, ()=>resolve()));
     await this.discord.logger.sendLog({
       type: "Info",
@@ -108,6 +121,14 @@ export class YoutubeClient extends YouTubeNotifier implements baseClient {
 
   private HealthRoute(req: Express.Request, res: Express.Response) {
     res.status(200).send("You have been OwO");
+  }
+
+  private errMiddleWare(err: MiddlewareError, req: http.IncomingMessage, res: http.ServerResponse, next: NextFunction) {
+    const statusCode = parseInt((err.status ?? err.statusCode ?? err.status_code ?? err.output?.statusCode ?? "500") as string, 10);
+    getIsolationScope().setSDKProcessingMetadata({ normalizedRequest: httpRequestToRequestData(req) });
+    if(statusCode >= 500)
+      (res as { sentry?: string }).sentry = captureException(err, { mechanism: { type: 'middleware', handled: false } });
+    next(err);
   }
 
   public async dispose() {
