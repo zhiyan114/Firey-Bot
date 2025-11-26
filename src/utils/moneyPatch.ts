36894a422237ec182c441d7d18afa301129ab1f4
zhiyan114 <zhiyan114@furries.dev>
2025-11-26 01:54:54 -0500

    Monkey Patch Modal boxes :3

--- src/utils/moneyPatch.ts
+++ src/utils/moneyPatch.ts
@@ -1,9 +1,23 @@
-// MoneyPatch functions
-import type { APIEmbed, Interaction, InteractionReplyOptions, MessageCreateOptions } from "discord.js";
-import { EmbedBuilder, MessagePayload } from "discord.js";
+// MonkeyPatch functions
+import type {
+  APIEmbed,
+  APIModalInteractionResponseCallbackData,
+  Interaction,
+  InteractionReplyOptions,
+  JSONEncodable,
+  MessageCreateOptions,
+  ModalComponentData,
+  ShowModalOptions
+} from "discord.js";
+import {
+  EmbedBuilder,
+  MessagePayload,
+  ModalBuilder,
+  TextDisplayBuilder
+} from "discord.js";
 
 
-export function moneyPatchReqID(interaction: Interaction, reqID: string) {
+export function MonkeyPatchReqID(interaction: Interaction, reqID: string) {
   // Content Patcher
   function patchContent(options: MessagePayload | MessageCreateOptions | InteractionReplyOptions) {
     if(options instanceof MessagePayload) {
@@ -18,7 +32,7 @@ export function moneyPatchReqID(interaction: Interaction, reqID: string) {
     if(options.embeds && options.embeds.length > 0) {
       const newEmbed = new EmbedBuilder(options.embeds[0] as APIEmbed);
       newEmbed.addFields({ name: "Request ID", value: reqID });
-      // @ts-expect-error MoneyPatch
+      // @ts-expect-error MonkeyPatch
       options.embeds[0] = newEmbed;
       return;
     }
@@ -32,12 +46,12 @@ export function moneyPatchReqID(interaction: Interaction, reqID: string) {
     interaction.channel.send = async (options: string | MessagePayload | MessageCreateOptions) => {
       if(typeof(options) === "string") {
         options += `\n\nRequest ID: ${reqID}`;
-        // @ts-expect-error MoneyPatch
+        // @ts-expect-error MonkeyPatch
         return await oldSend.call(interaction.channel, options);
       }
 
       patchContent(options);
-      // @ts-expect-error MoneyPatch
+      // @ts-expect-error MonkeyPatch
       return await oldSend.call(interaction.channel, options);
     };
   }
@@ -46,7 +60,7 @@ export function moneyPatchReqID(interaction: Interaction, reqID: string) {
   if(interaction.isCommand() || interaction.isUserContextMenuCommand()) {
     // eslint-disable-next-line
     const oldReply = interaction.reply;
-    // @ts-expect-error MoneyPatch
+    // @ts-expect-error MonkeyPatch
     // eslint-disable-next-line
     interaction.reply = async function(options: string | MessagePayload | InteractionReplyOptions) {
       if(typeof(options) === "string") {
@@ -68,6 +82,23 @@ export function moneyPatchReqID(interaction: Interaction, reqID: string) {
       patchContent(options);
       return await oldFollowUp.call(interaction, options);
     };
+
+    // Patch Modal box
+    const oldShowModal = interaction.showModal;
+    // @ts-expect-error MonkeyPatch
+    interaction.showModal = async function(modal: JSONEncodable<APIModalInteractionResponseCallbackData> | ModalComponentData | APIModalInteractionResponseCallbackData, options?: ShowModalOptions & { withResponse: true;}) {
+      // @ts-expect-error MonkeyPatch
+      const newModal = new ModalBuilder(modal.toJSON ? modal.toJSON() : modal);
+
+      // Add reqID label at the end of the modal
+      newModal.addTextDisplayComponents(
+        new TextDisplayBuilder()
+          .setId(99999) // last order ig
+          .setContent(`Request ID: ${reqID}`)
+      );
+
+      return await oldShowModal.call(interaction, newModal, options);
+    };
   }
 
   // Patch user message
@@ -81,4 +112,5 @@ export function moneyPatchReqID(interaction: Interaction, reqID: string) {
     patchContent(options);
     return await oldUserSend.call(interaction.user, options);
   };
+
 }
