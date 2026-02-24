e6b331931c04b218e66279ef5c15f26d3cbef0d0
zhiyan114 <zhiyan114@furries.dev>
2026-02-23 20:00:09 -0500

    Dont execute grantPoints OP if no points are given..

--- src/services/VoiceChatReward.ts
+++ src/services/VoiceChatReward.ts
@@ -228,6 +228,8 @@ class _internalUser {
 
   public async computeReward() {
     const rewardCount = Math.floor(this.secCounted / 600); // RNG points per 10 minutes
+    if(rewardCount === 0) return;
+
     let points = 0;
     // Grant 7-11 points per rewardCount
     for(let i = 0; i < rewardCount; i++)
