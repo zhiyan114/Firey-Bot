41d69d933fad93aea2b555b98a01c342f6f9e7b7
zhiyan114 <zhiyan114@zhiyan114.com>
2026-04-03 20:48:35 -0400

    Resolve HTTP/2 'GOAWAY' issue due to extensive keep-alive

--- src/core/helper/twitchStream.ts
+++ src/core/helper/twitchStream.ts
@@ -7,7 +7,7 @@
 import type { TwitchClient } from "../TwitchClient";
 import { captureException } from "@sentry/node-core";
 import events from "events";
-import { fetch, Agent, errors } from "undici";
+import { errors, Pool } from "undici";
 import { sendLog } from "../../utils/eventLogger";
 
 // Type Reference: https://dev.twitch.tv/docs/api/reference#get-streams
@@ -51,7 +51,7 @@ export class streamClient extends events.EventEmitter {
   private cooldown: number;
   public isStreaming = false;
   private errLogged = false;
-  private webAgent: Agent;
+  private webPool: Pool;
 
   constructor(client: TwitchClient, channelName: string, cooldown?: number, token?: string) {
     super();
@@ -62,11 +62,17 @@ export class streamClient extends events.EventEmitter {
     this.channel = channelName;
     this.cooldown = cooldown ?? 30000; // Check every 30 seconds by default
 
-    // Create a dedicated fetch Agent instance for this request to fix ETIMEDOUT (probably due to SNAT issue)
-    this.webAgent = new Agent({
+    /* The original solution was to use Agent, which fixed ETIMEDOUT
+    (possibly related to exausted SNAT port due to opening connections frequently)
+    The new solution uses Pool to to resolve both the ETIMEOUT error and "GOAWAY" (HTTP/2) request from the server
+    by ensuring that a new connection doesn't open every 30s and dies before 1 hour mark */
+    this.webPool = new Pool("https://api.twitch.tv",{
+      connections: 1,
+      allowH2: true,
+      clientTtl: 45*60*1000, // 45 min
       connect: {
         keepAlive: true,
-        timeout: Math.ceil(this.cooldown / 2),
+        timeout: Math.ceil(this.cooldown / 2)
       }
     });
 
@@ -75,17 +81,17 @@ export class streamClient extends events.EventEmitter {
 
   private mainCheck = async () => {
     try {
-      const serverRes = await fetch(`https://api.twitch.tv/helix/streams?user_login=${this.channel}`, {
+      const serverRes = await this.webPool.request<twitchGetStreamType>({
+        path: `/helix/streams?user_login=${this.channel}`,
         method: "GET",
         headers: {
           "Client-ID": "q6batx0epp608isickayubi39itsckt", // Just using someone else's client ID
           "Authorization": `Bearer ${this.token}`,
         },
-        dispatcher: this.webAgent,
       });
 
-      const contentType = serverRes.headers.get("content-type");
-      if(!contentType || contentType.includes("application/json") === false) {
+      const contentType = serverRes.headers["content-type"];
+      if(!contentType || !contentType.includes("application/json")) {
         return await sendLog({
           type: "Warning",
           message: `Twitch API has responded with invalid content type header: \`${contentType}\``
@@ -93,13 +99,13 @@ export class streamClient extends events.EventEmitter {
       }
 
 
-      const bodyData = await serverRes.json() as twitchGetStreamType;
-      if(!serverRes.ok)
+      const bodyData = await serverRes.body.json() as twitchGetStreamType;
+      if(serverRes.statusCode < 200 || serverRes.statusCode >= 300)
         return await sendLog({
           type: "Warning",
           message: `twitchStream: Status code did not respond with OK body: \`\`\`${bodyData}\`\`\``,
           metadata: {
-            status: serverRes.status.toString(),
+            status: serverRes.statusCode.toString(),
           }
         });
 
