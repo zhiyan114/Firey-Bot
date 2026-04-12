4ee9e6a876e52ea9d04d21d79090b5f9be6ac3a8
zhiyan114 <zhiyan114@zhiyan114.com>
2026-04-12 16:04:34 -0400

    redis span use config-based value instead of hardcode

--- src/loader.ts
+++ src/loader.ts
@@ -90,7 +90,7 @@ function beforeBreadcrumb(breadcrumb: Breadcrumb) {
 
 function beforeSendTransaction(event: TransactionEvent) {
   if(event.transaction?.startsWith("prisma")) return null; // Drop prisma root span
-  if(event.transaction?.startsWith("FireyBot:")) return null; // Drop redis root span
+  if(event.transaction?.startsWith(`${redisPrefix}:`)) return null; // Drop redis root span
   if(!event.spans || event.spans.length < 2) return null; // Spans with only transaction tend to useless
   return event;
 }
